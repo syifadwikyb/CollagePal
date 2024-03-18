25fb5de007bc0270bc65895b06e704049bfead14
Syifa Dwiky Basamala <syifadwikyb@gmail.com>
2024-03-18 23:41:45 +0700

    first commit

---
+++ app/src/main/java/com/example/home/belum_selesai.java
@@ -0,0 +1,14 @@
+package com.example.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class belum_selesai extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_belum_selesai);
+    }
+}
--- app/src/main/java/com/example/home/tugas1.java
+++ app/src/main/java/com/example/home/tugas1.java
@@ -1,12 +1,10 @@
 package com.example.home;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 
 public class tugas1 extends AppCompatActivity {
@@ -26,28 +24,14 @@ public class tugas1 extends AppCompatActivity {
             }
         });
     }
+
     private void showPopup() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Input Dialog");
+
 
         // Inflate layout untuk dialog
         View view = getLayoutInflater().inflate(R.layout.activity_popup_layout, null);
         builder.setView(view);
-
-        // Set up tombol pada dialog
-        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                // Aksi ketika tombol OK ditekan
-                // Anda dapat menangani input pengguna dari dialog di sini
-            }
-        });
-        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.dismiss(); // Tutup dialog ketika tombol Cancel ditekan
-            }
-        });
 
         // Munculkan dialog
         AlertDialog dialog = builder.create();
