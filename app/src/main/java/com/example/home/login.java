02fe7aa27ed38f79fe775ec2a3c8bcd65dcc834e
Syifa Dwiky Basamala <syifadwikyb@gmail.com>
2024-03-19 11:33:11 +0700

    first commit

--- app/src/main/java/com/example/home/login.java
+++ app/src/main/java/com/example/home/login.java
@@ -6,10 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class login extends AppCompatActivity {
 
     private Button signIn;
+    private TextView signUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +19,19 @@ public class login extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         signIn = findViewById(R.id.signIn);
+        signUp = findViewById(R.id.signUp);
 
         signIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent (login.this, tugas1.class);
+                startActivity(intent);
+            }
+        });
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (login.this, signUp.class);
                 startActivity(intent);
             }
         });
